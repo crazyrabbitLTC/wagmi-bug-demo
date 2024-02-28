596d8ae9aa93b1fca14c47a04a29f03270fb3ed5
Dennison Bertram <dennison@dennisonbertram.com>
2024-02-27 22:48:49 -0500

    example not working on base

---
+++ src/app/page.tsx
@@ -0,0 +1,80 @@
+'use client'
+
+import { useAccount, useConnect, useDisconnect, useSendTransaction } from 'wagmi'
+import { parseEther} from 'viem'
+
+function App() {
+  const account = useAccount()
+  const { connectors, connect, status, error } = useConnect()
+  const { disconnect } = useDisconnect()
+
+  return (
+    <>
+      <div>
+        <h2>Account</h2>
+
+        <div>
+          status: {account.status}
+          <br />
+          addresses: {JSON.stringify(account.addresses)}
+          <br />
+          chainId: {account.chainId}
+        </div>
+
+        {account.status === 'connected' && (
+          <button type="button" onClick={() => disconnect()}>
+            Disconnect
+          </button>
+        )}
+      </div>
+      <SendTransaction/>
+
+      <div>
+        <h2>Connect</h2>
+        {connectors.map((connector) => (
+          <button
+            key={connector.uid}
+            onClick={() => connect({ connector })}
+            type="button"
+          >
+            {connector.name}
+          </button>
+        ))}
+        <div>{status}</div>
+        <div>{error?.message}</div>
+      </div>
+    </>
+  )
+}
+function SendTransaction() {
+  const { 
+    data: hash, 
+    isPending, 
+    sendTransaction 
+  } = useSendTransaction() 
+
+  async function submit(e: React.FormEvent<HTMLFormElement>) { 
+    e.preventDefault() 
+    const formData = new FormData(e.target as HTMLFormElement) 
+    const to = formData.get('address') as `0x${string}` 
+    const value = formData.get('value') as string 
+    sendTransaction({ to, value: parseEther(value) }) 
+  } 
+
+  return (
+    <form onSubmit={submit}>
+      <input name="address" placeholder="0xA0Cf…251e" required />
+      <input name="value" placeholder="0.05" required />
+      <button 
+        disabled={isPending} 
+        type="submit"
+      >
+        Send 
+        {isPending ? 'Confirming...' : 'Send'} 
+      </button>
+      {hash && <div>Transaction Hash: {hash}</div>} 
+    </form>
+  )
+}
+
+export default App
